d77c70068caaac3160be0b5564ca10d9744cac56
aaz-cell <18052913209@163.com>
2026-07-20 11:22:53 +0800

    Fix portable iSweep launch wrapper

--- sentry_nav/scripts/launch_isweep_clean.sh
+++ sentry_nav/scripts/launch_isweep_clean.sh
@@ -7,8 +7,4 @@ source "${SCRIPT_DIR}/ros_noetic_env.sh"
 
 cd "${WORKSPACE_DIR}"
 
-exec roslaunch sentry_nav navigation_isweep_switch.launch \
-  run_legacy_move_base:=false \
-  enable_isweep_cmd_output:=false \
-  cmd_vel_topic:=/isweep_cmd_vel \
-  "$@"
+exec roslaunch sentry_nav navigation_isweep_switch.launch "$@"
